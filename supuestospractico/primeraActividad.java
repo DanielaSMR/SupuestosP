8252eb641229a34cbb2cb9cd5e0fc59fee68354a
DanielaSMR <10828775@ieselgrao.org>
2023-09-26 12:13:40 +0200

    g

---
+++ supuestospractico/primeraActividad.java
@@ -0,0 +1,18 @@
+package supuestospractico;
+
+public class primeraActividad {
+    
+    public static void main(String[] args){
+
+        final int CONST_NUM_ENTERO = 1008;
+        final String CONST_LETRA = "Saludos";
+
+        Integer variableEnteros1 = CONST_NUM_ENTERO;
+        Integer variableEnteros2 = variableEnteros1;
+
+        System.out.println(variableEnteros1 + " " + variableEnteros2 + " " + CONST_LETRA);
+
+
+
+     }
+}
