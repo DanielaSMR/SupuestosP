0ea6cf35037e2ad42d9ca515edbab9f9ac632451
DanielaSMR <120648177+DanielaSMR@users.noreply.github.com>
2023-09-30 16:23:28 +0000

    guarda

---
+++ EjerciciosBasicos/Ejer1Inicial.java
@@ -0,0 +1,22 @@
+package EjerciciosBasicos;
+import java.util.*;
+
+
+public class Ejer1Inicial{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = 5;
+        double A = 6;
+        char C = 'A';
+        int x = C;
+
+        System.out.println(N);
+        System.out.println(A);
+        System.out.println(C);
+        System.out.println(x);
+        System.out.println(N + A);
+        System.out.println(A - N);
+    }
+}
