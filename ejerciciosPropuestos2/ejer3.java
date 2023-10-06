08c6030b8efa1c1bae4bdb9cfeeb334cae633978
DanielaSMR <10828775@ieselgrao.org>
2023-10-06 13:12:22 +0200

    g

--- ejerciciosPropuestos2/ejer3.java
+++ ejerciciosPropuestos2/ejer3.java
@@ -7,22 +7,39 @@ public class ejer3 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Como de alta quieres la piramide?");
-        int alturaMax = Integer.parseInt(args[0]);
+        int alturaMax = sc.nextInt();
         int nFinal = (alturaMax * 2) + 1;
         int espaciosPrimer = nFinal / 2; 
         int nS = 1;
+        int alturaMax2 = alturaMax;
+        int guardarEspacio = espaciosPrimer;
         
-        while(alturaMax > 0){
+        while(alturaMax/2 > 0){
                 for(int x = espaciosPrimer; x > 0; x--){
-                System.out.print(" ");//el primer for nos añade los espacios
+                System.out.print("X");
+                x -=1;
                 }
                 for(int y = nS ;y > 0; y--){
-                System.out.print("N");//el segundo nos pone las N
+                System.out.print("N");
                 }
                 nS = nS + 2;
                 espaciosPrimer -= 1;
                 alturaMax = alturaMax - 1;
                 System.out.print("\n");
         }
+        while(alturaMax2/2 > 0){
+                for(int x = guardarEspacio; x > alturaMax2; x--){
+                System.out.print("X");
+                x -=1;
+                }
+                for(int y = nS ;y > 0; y--){
+                System.out.print("N");
+                }
+                nS = nS - 2;
+                guardarEspacio +=1;
+                alturaMax2 = alturaMax2 - 1;
+                System.out.print("\n");
+        }
+
      }
 }
---
+++ ejerciciosPropuestos2/test.java
@@ -0,0 +1,12 @@
+package ejerciciosPropuestos2;
+import java.util.*;
+import java.util.stream.IntStream;
+public class test {
+    public static void main ( String [ ] args) {
+    Random rnd = new Random();
+    int valor = rnd.nextInt() * 200 + 100;
+    System.out.println(valor);
+    }
+}
+
+
