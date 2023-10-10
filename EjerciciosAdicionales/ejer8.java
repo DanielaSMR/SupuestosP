0c7e243af2200d4fab7c5f3a23a40340de1ccc8f
DanielaSMR <10828775@ieselgrao.org>
2023-10-10 13:03:55 +0200

    guat

--- EjerciciosAdicionales/ejer8.java
+++ EjerciciosAdicionales/ejer8.java
@@ -6,12 +6,43 @@ public class ejer8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Introduce tu dia de nacimiento");
+        System.out.println("Introduce el dia");
         int dia = sc.nextInt();
-        System.out.println("Introduce tu mes de nacimiento");
+        System.out.println("Introduce el mes");
         int mes = sc.nextInt();
-        System.out.println("Introduce tu año de nacimiento");
-        int año = sc.nextInt();
+        System.out.println("Introduce el año");
+        int anyo = sc.nextInt();
+
+        if(mes == 1 && dia < 31 && anyo <=2023){
+            System.out.println("Fecha valida");
+        }else{
+            System.out.println("No valido");
+        }
+        if(mes == 2 && dia < 28 && anyo <=2023){
+            System.out.println("Fecha valida");
+        }else{
+            System.out.println("No valido");
+        }
+        if(mes == 3 && dia < 31 && anyo <=2023){
+            System.out.println("Fecha valida");
+        }else{
+            System.out.println("No valido");
+        }
+        if(mes == 4 && dia < 30 && anyo <=2023){
+            System.out.println("Fecha valida");
+        }else{
+            System.out.println("No valido");
+        }
+        if(mes == 5 && dia < 31 && anyo <=2023){
+            System.out.println("Fecha valida");
+        }else{
+            System.out.println("No valido");
+        }
+        if(mes == 6 && dia < 31 && anyo <=2023){
+            System.out.println("Fecha valida");
+        }else{
+            System.out.println("No valido");
+        }
 
     }
 }
