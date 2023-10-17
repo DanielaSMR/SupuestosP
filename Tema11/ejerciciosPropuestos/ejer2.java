464d7c89fb01ed54159a4482592f9ac4e1d6cbb6
DanielaSMR <10828775@ieselgrao.org>
2023-10-17 12:26:26 +0200

    g

--- Tema11/ejerciciosPropuestos/ejer2.java
+++ Tema11/ejerciciosPropuestos/ejer2.java
@@ -8,19 +8,43 @@ public class ejer2 {
 
         System.out.println("Escribe el numero");
         int numeroFibonacci = sc.nextInt();
-        //System.out.println(numeroFactorial+ "*" + numeroFactorial + "=" + numeroFactorial * numeroFactorial);
-        System.out.println("Recursivo");
-        fibonacci_recursivo(numeroFibonacci);
+        int y = 2;
+        int x = 1;
         System.out.println("Iterativo");
-        //factorialIterativo(numeroFibonacci);
+        fibonacci_iterativo(numeroFibonacci);
+        System.out.println("Recursivo");
+        fibonacci_recursivo(numeroFibonacci, y, x);
     }
 
-    public static Integer fibonacci_recursivo(int num){
-        if(num == 1){
-            return 1;
-        }else{
-            System.out.println(num);
-            return fibonacci_recursivo(num - 1 + (num));
+    public static Integer fibonacci_iterativo(int num){
+        int numIterativo = 0;
+        int num2 = 1;
+        for(int i = 0;i <= num - 3; i++){
+            if(numIterativo == 0){
+                System.out.println(numIterativo);
+                numIterativo = numIterativo + 1;
+                System.out.println(numIterativo);
+            }else if(numIterativo == 1){
+                numIterativo = numIterativo + 1;
+                System.out.println(numIterativo);
+            }
+            int suma = numIterativo + num2;
+            num2 = numIterativo;
+            numIterativo = suma;
+            System.out.println(numIterativo);
         }
+        return 0;
     }  
+
+    public static Integer fibonacci_recursivo(int num, int y, int x){
+        if(num >= 3){
+            int suma = y + x;
+            System.out.println(y);
+            return fibonacci_recursivo(num - 1, suma, y);
+        }else{
+            return 0;
+        }
+        
+    }
+    
 }
