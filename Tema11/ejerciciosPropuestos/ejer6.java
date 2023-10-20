f5f6553f94da99f305463bf2c85b63d0e0744d91
DanielaSMR <10828775@ieselgrao.org>
2023-10-20 12:12:15 +0200

    g

--- Tema11/ejerciciosPropuestos/ejer6.java
+++ Tema11/ejerciciosPropuestos/ejer6.java
@@ -1,7 +1,26 @@
 package Tema11.ejerciciosPropuestos;
 
+import java.util.Scanner;
+
 public class ejer6 {
+
+    public static Integer funcionBinario(int num, int numMantener){
+        if(num == 0){
+            return 0;
+        }else{
+            num = num % 2;// te da el 0 o 1
+            System.out.println(num);
+            int division = numMantener / 2;// va bajando el numero
+            return funcionBinario(division, division);
+        }
+    }
+    
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escribe el numero que quieres");
+        int num = sc.nextInt();
+        funcionBinario(num, num);
         
     }
 }
