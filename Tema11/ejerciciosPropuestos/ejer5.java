2664ec7f089f2d4268948749cfbcda7d6c7dc321
DanielaSMR <10828775@ieselgrao.org>
2023-10-22 19:15:23 +0200

    guardar

--- Tema11/ejerciciosPropuestos/ejer5.java
+++ Tema11/ejerciciosPropuestos/ejer5.java
@@ -1,7 +1,45 @@
 package Tema11.ejerciciosPropuestos;
 
+import java.util.Scanner;
+
 public class ejer5 {
+    public static Integer funcion(int f, int c){
+        if(f==1){
+            return 1;
+        }
+        if (c==1){
+            return 1;
+        }
+        if(f % 2 == 0 && c % 2 == 0){
+            return 0; 
+        }else if(f % 2 == 1 && c % 2 == 0){
+            return 0;
+        } else if(c == f * 2 - 2){
+            return 0;
+        }else if (c == 2){
+            return 0;
+        } else if(c==f * 2 -1){
+            return 1;
+        } else {
+            return funcion(f - 1, c - 2) + funcion(f - 1, c - 1) + funcion(f - 1, c);
+        }
+       
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Cuantas lineas quieres?");
+        int alturaMax = sc.nextInt();
+
+        for(int f = 1;f <= alturaMax;f++){
+            for(int e = alturaMax; e >= f; e--){
+                System.out.print(" " + "\t");
+                
+            }
+            for(int c = 1;c <= f * 2 - 1;c++){
+                System.out.print(funcion(f, c) + "\t");
+            }
+            System.out.println(" ");
+        }
     }
 }
--- Tema11/ejerciciosPropuestos/ejer6.java
+++ Tema11/ejerciciosPropuestos/ejer6.java
@@ -14,6 +14,7 @@ public class ejer6 {
             return funcionBinario(division);
         }
     }
+
     public static Integer funcionOctal(int num){
         if(num == 0){
             return 0;
@@ -21,10 +22,34 @@ public class ejer6 {
             int resto = num % 8;
             System.out.println(resto);
             int division = num / 8;
-            return funcionBinario(division);
+            return funcionOctal(division);
         }
     }
 
+    public static Integer funcionHexa(int num){
+        if(num == 0){
+            return 0;
+        }else{
+            int resto = num % 16;
+            if(resto == 10){
+                System.out.println("A");
+            }else if(resto == 11){
+                System.out.println("B");
+            }else if(resto == 12){
+                System.out.println("C");
+            }else if(resto == 13){
+                System.out.println("D");
+            }else if(resto == 14){
+                System.out.println("E");
+            }else if(resto == 15){
+                System.out.println("F");
+            }else{
+                System.out.println(resto);
+            }
+            int division = num / 16;
+            return funcionHexa(division);
+        }
+    }
     
     
     
@@ -33,9 +58,12 @@ public class ejer6 {
 
         System.out.println("Escribe el numero que quieres");
         int num = sc.nextInt();
+        System.out.println("Leer de abajo a arriba :)");
         System.out.println("Binario:");
         funcionBinario(num);
         System.out.println("Octal:");
         funcionOctal(num);
+        System.out.println("Hexadecimal");
+        funcionHexa(num);
     }
 }
