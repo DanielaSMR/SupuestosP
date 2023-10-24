3ebc6fba1ea2620a9cb9f69a6faafc8bf7a4924d
DanielaSMR <10828775@ieselgrao.org>
2023-10-24 13:02:21 +0200

    guardar

--- Tema11/ejerciciosPracticos/ejer3.java
+++ Tema11/ejerciciosPracticos/ejer3.java
@@ -4,10 +4,14 @@ import java.util.Scanner;
 
 public class ejer3 {
 
-    public static String funcionBorrar(String p){
-        int contarLetras = p.length();
-        if(contarLetras > 0){
-            
+    public static String funcionBorrar(String p, int x){
+        if(x == 0){
+            System.out.println(p.charAt(0));
+        }else{
+            //System.out.println(p.charAt(0));
+            //System.out.println(p.length());
+            System.out.print(p.charAt(p.length() - 1));
+            //return funcionBorrar(p, x - 1);
         }
         return " ";
     }
@@ -17,7 +21,16 @@ public class ejer3 {
 
         System.out.println("Escribe lo que quieras");
         String palabra = sc.nextLine();
+        String y = palabra;
+        int x = y.length();
+        funcionBorrar(palabra, x);
 
-        
+        for(int i = 1;i <= y.length();i++){
+            for(int a = 0;a <= x;a++){
+                funcionBorrar(palabra, x);
+                System.out.println("\t");
+            }
+            x--;
+        }
     }
 }
