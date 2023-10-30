fa36225176b0f72f591764e60900ed705f3b4807
DanielaSMR <10828775@ieselgrao.org>
2023-10-30 14:56:15 +0100

    guar

--- Tema8/ejer2.java
+++ Tema8/ejer2.java
@@ -1,5 +1,14 @@
 public class ejer2 {
     public static void main(String[] args) {
-        
+        int []num;
+        num = new int[100];
+        int suma = 0;
+
+        for(int i = 0;i < 100;i++){
+            num [i] = (int) (Math.random()* 999);
+            System.out.println(num[i]);
+            suma = suma + num[i];
+        }
+        System.out.println("Suma total " + suma);
     }
 }
