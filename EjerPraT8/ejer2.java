ec2a6a4425b6da12032341047ad412dd7ce6ac70
DanielaSMR <10828775@ieselgrao.org>
2023-11-08 14:53:45 +0100

    g

--- EjerPraT8/ejer2.java
+++ EjerPraT8/ejer2.java
@@ -4,15 +4,30 @@ import java.util.Arrays;
 
 public class ejer2 {
     public static void main(String[] args) {
-        int []walter;
+        int []num;
         num = new int[100];
+        int suma = 0;
+        int comparar = 0;
+        int comparar2 = 0;
+        int guardarNumMayor = 0;
+        int guardarNumMenor = 90;
 
         for(int i = 0;i < 100;i++){
             num [i] = (int) (Math.random()*71+10);
+            suma = suma + num[i];
+            comparar = num[i];
+            comparar2 = num[i];
+            if(comparar > guardarNumMayor){
+                guardarNumMayor = comparar;
+            }
+            if(comparar2 < guardarNumMenor){
+                guardarNumMenor = comparar2;
+            }
         }
 
-        Arrays.sort(num);
-        System.out.println(Arrays.toString(num));
+        System.out.println(guardarNumMayor);
+        System.out.println(guardarNumMenor);
+        System.out.println(suma / 100);
         //int maximo = Math.max(num[0], num[100]);
         //System.out.println(maximo);
     }
---
+++ EjerPraT8/ejer3.java
@@ -0,0 +1,16 @@
+package EjerPraT8;
+
+public class ejer3 {
+    public static void main(String[] args) {
+        int [][]matriz;
+        matriz = new int[5][8];
+
+        int e = 0;
+
+        for(int i = 0;i < 5;i++){
+            matriz [i][e] = (int) (Math.random()* 100);
+            for(e = 0;e < 8;e++){}
+                e = e + 1;
+            }
+        }
+    }
