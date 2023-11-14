70365c101a0317d1cb6613e26bc2f8de207410e3
DanielaSMR <10828775@ieselgrao.org>
2023-11-14 23:35:38 +0100

    g

--- EjerPraT8/ejer8.java
+++ EjerPraT8/ejer8.java
@@ -1,11 +1,27 @@
 package EjerPraT8;
 
+import java.util.Arrays;
+
 public class ejer8 {
     public static void main(String[] args) {
-        String palabras = "Sergio\n8.5\nMario\n4.9\nPedro\n3.8\nAndrés\n6.3";
-        String [] cadena = palabras.split("\n", 0);
+        String cadena = "Sergio\n8.5\nMario\n4.9\nPedro\n3.8\nAndrés\n6.3";
 
-        System.out.prinln(cadena);
+        String [] palabras = cadena.split("\n");
+        String [] nombres = new String[palabras.length/2];
+        Double [] notas = new Double[palabras.length/2];
+
+        
+    for(int i = 0;i < palabras.length; i++){
+        if(i%2==0){
+            nombres[i/2] = palabras[i];
+        } else {
+            notas[i/2] = Double.parseDouble(palabras[i]);
+        }
+    }
+
+    for(int e = 0; e < nombres.length; e++){
+        System.out.println(nombres[e] + " ha sacado un " + notas[e]);
+    }
 
         
     }
--- Tema8/ejer4.java
+++ Tema8/ejer4.java
@@ -1,10 +1,26 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ejer4 {
     public static void main(String[] args) {
-
-        String cadena = args[0];
-        System.out.printn(cadena.split("\\s+|\n").length);
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("Escribe");
+        String texto = sc.nextLine();
+        int contador = 1;
+        
+        texto = texto.toLowerCase();
+
+        String[] palabras = texto.split(" ");
+
+        System.out.println(Arrays.toString(palabras));
+        Arrays.sort(palabras);
+        for(int i = 0; i < palabras.length - 1; i++){
+            if (!palabras[i + 1].equals(palabras[i])){
+                contador = contador + 1;
+            }
+        }
+
+        System.out.println(contador);
     }
 }
